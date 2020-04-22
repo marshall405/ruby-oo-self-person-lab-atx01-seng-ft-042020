12c4b62a0308563fd70565b6b2fa8ed6438c6b7e
marshall405 <marshall.slemp@gmail.com>
2020-04-22 15:14:28 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,77 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = set_min_max(num)
+    end
+
+    def hygiene=(num)
+        @hygiene = set_min_max(num)
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = @hygiene + 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene = @hygiene - 3
+        self.happiness = @happiness + 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness = @happiness + 3
+        friend.happiness = friend.happiness + 3
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            self.happiness = @happiness - 2
+            friend.happiness = friend.happiness - 2
+            return "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness = @happiness + 1
+            friend.happiness = friend.happiness + 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
+
+
+    private
+    def set_min_max(num)
+        if num > 10
+            num = 10
+        elsif num < 0
+            num = 0
+        end
+        num 
+    end
+
+end
